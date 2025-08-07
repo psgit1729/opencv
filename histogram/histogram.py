07fc70d384d04fb5d2e7e95413c33500af88ff02
psgit1729 <pskaladhar@gmail.com>
2025-08-07 12:40:39 +0530

    hbh

--- histogram/histogram.py
+++ histogram/histogram.py
@@ -1,6 +1,21 @@
 import cv2
+import matplotlib.pyplot as plt
 
 
 #### read image from folder
 
 img=cv2.imread("image.png")
+
+
+### apply histogram function
+
+hist=cv2.calcHist([img],[0],None,[256],[0,266])
+
+
+## plot the histogram
+plt.plot(hist)
+plt.show()
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
