9bb0738a8d4b805c3fbe2794f25527f4c079e3af
psgit1729 <pskaladhar@gmail.com>
2025-08-07 13:01:45 +0530

    hvbhdc

---
+++ histogram/histogram_loop.py
@@ -0,0 +1,37 @@
+import cv2
+import matplotlib.pyplot as plt
+
+
+#### read image from folder
+
+img=cv2.imread("red.png")
+
+
+### apply histogram function
+x=[0,1,2]
+store_his=[]
+
+for i in x:
+    hist=cv2.calcHist([img],[i],None,[256],[0,266])
+    store_his.append(hist)
+    
+    
+#plot the graph individually
+
+plt.plot(store_his[0],color="blue")
+plt.plot(store_his[1],color="green")
+plt.plot(store_his[2],color="red")
+
+#plt.plot(hist)
+plt.show()
+
+
+
+
+## plot the histogram
+
+
+
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
